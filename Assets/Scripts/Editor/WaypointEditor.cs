4c51748dab06580f9e79c77c23d42a1522b10b20
ccaian <ccaian3@gmail.com>
2021-09-30 17:26:32 +0800

    Version 1.0 update
    -Base layout of city constructed
    -Car (Free Retro Car Yellow) Prefabs Added

---
+++ Assets/Scripts/Editor/WaypointEditor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad()]
+public class WaypointEditor
+{
+    [DrawGizmo(GizmoType.NonSelected | GizmoType.Selected | GizmoType.Pickable)]
+    public static void OnDrawSceneGizmo(Waypoint waypoint, GizmoType gizmoType)
+    {
+        if ((gizmoType & GizmoType.Selected) != 0)
+        {
+            Gizmos.color = Color.yellow;
+        }
+        else
+        {
+            Gizmos.color = Color.yellow * 0.5f;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/WaypointManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WaypointManager : EditorWindow
+{
+    [MenuItem("Tools/Waypoint Editor")]
+    public static void Open()
+    {
+        GetWindow<WaypointManager>();
+    }
+
+    public Transform waypointRoot;
+
+    private void OnGUI()
+    {
+        SerializedObject obj = new SerializedObject(this);
+        EditorGUILayout.PropertyField(obj.FindProperty("waypointRoot"));
+
+        if (waypointRoot == null)
+        {
+            EditorGUILayout.HelpBox("Please assign a root transform", MessageType.Warning);
+        }
+        else
+        {
+            EditorGUILayout.BeginVertical("box");
+            DrawButtons();
+            EditorGUILayout.EndVertical();
+        }
+        obj.ApplyModifiedProperties();
+    }
+    void DrawButtons()
+    {
+        if (GUILayout.Button("Create Waypoint"))
+        {
+            CreateWaypoint();
+        }
+    }
+    void CreateWaypoint()
+    {
+        GameObject waypointObject = new GameObject("Waypoint " + waypointRoot.childCount, typeof(Waypoint));
+        waypointObject.transform.SetParent(waypointRoot, false);
+
+        Waypoint waypoint = waypointObject.GetComponent<Waypoint>();
+        if (waypointRoot.childCount > 1)
+        {
+        
+            waypoint.previousWaypoint = waypointRoot.GetChild(waypointRoot.childCount - 2).GetComponent<Waypoint>();
+            waypoint.previousWaypoint.nextWaypoint = waypoint;
+            waypoint.transform.position = waypoint.previousWaypoint.transform.position;
+            waypoint.transform.forward = waypoint.previousWaypoint.transform.forward;
+        }
+
+        Selection.activeGameObject = waypoint.gameObject;
+    }
+}
